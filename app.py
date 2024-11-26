b0d34040276df85cbc79566fd9780a5c3d81e66d
szewczem <szewczem@gmail.com>
2024-11-26 19:03:52 +0100

    Revert "Merge pull request #2 from szewczem/featurePiotrek"
    This reverts commit de8eabbf68a298cc4d575e1efcf56caecc25d5d5, reversing
    changes made to fb22d04ca38fae5334a0536feb845aef6c6a61e7.

--- app.py
+++ app.py
@@ -5,19 +5,3 @@ if wiek.isdigit() == False:
 wiek=int(wiek)
 if wiek>=18:
     print("Witamy w apce. Mozesz kupować u nas energetyki")
-
-#task 1 wiek powyzej 120 lat
-#dodatkowo wiek ponizej 18 lat
-
-wiek = input("Podaj wiek uzytkownika: ")
-#sprawdzenie czy wiek jest liczba calkowitą
-if wiek.isdigit() == False:
-    exit("Wiek musi byc liczba calkowita. Zamykam aplikację")
-wiek=int(wiek)
-if wiek>=18 and wiek<=120:
-    print("Witamy w apce. Mozesz kupować u nas energetyki")
-elif wiek<18:
-    print("Nie masz jeszcze 18 lat, nie kupisz u nas energetykow")
-else:
-    print("Masz ponad 120 lat, juz nie zyjesz")
-
