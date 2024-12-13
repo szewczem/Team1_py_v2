fcc88cadfcc19d3b4dc5cbd14c6a352f042bc9ba
aprzychodzka <alicja.przychodzka@gmail.com>
2024-12-14 00:25:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,15 @@ if wiek.isdigit() == False:
 wiek=int(wiek)
 if wiek>=18:
     print("Witamy w apce. Mozesz kupować u nas energetyki")
+
+
+
+# Alicja aprzychodzka // alicja.przychodzka@gmail.com  odradzenie dla osób +60 prozdrowotne
+
+def wiek_60 (wiek):
+    if wiek >= 60:
+        print("Energetyki nie są rekomendowane dla osób 60+.")
+    else:
+        pass
+
+wiek_60 (wiek) # wywołanie funkcji
